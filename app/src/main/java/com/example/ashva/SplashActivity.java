f954ade207148e09be679620bc9dbab911120816
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-05-28 02:13:04 +0530

    Adding the horizontal progress bar in the splash screen commit

--- app/src/main/java/com/example/ashva/SplashActivity.java
+++ app/src/main/java/com/example/ashva/SplashActivity.java
@@ -3,18 +3,50 @@ package com.example.ashva;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private ProgressBar progressBar;
+    private int progressStatus = 0;
+    private Handler handler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_activity);
-        new Handler().postDelayed(() -> {
+        handler.postDelayed(() -> {
             startActivity(new Intent(SplashActivity.this, MainActivity.class));
             finish();
         }, 2500);
+
+        // Setting up the progress bar
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        // Start long running operation in a background thread
+        new Thread(new Runnable() {
+            public void run() {
+                while (progressStatus < 100) {
+                    progressStatus += 1;
+                    // Update the progress bar and display the
+                    //current value in the text view
+                    handler.post(new Runnable() {
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                        }
+                    });
+                    try {
+                        // Sleep for 25 milliseconds.
+                        Thread.sleep(25);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
     }
 }
