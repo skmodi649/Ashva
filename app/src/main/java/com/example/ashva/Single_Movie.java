4729857261fd236eb6c5a8d53b25c743fc7d15ad
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-06-01 16:49:35 +0530

    UI changes commit

--- app/src/main/java/com/example/ashva/Single_Movie.java
+++ app/src/main/java/com/example/ashva/Single_Movie.java
@@ -1,10 +1,13 @@
 package com.example.ashva;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.squareup.picasso.Picasso;
@@ -14,6 +17,22 @@ public class Single_Movie extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_movie);
+
+        // Changing the color of action bar of the following activity
+        // Define ActionBar object
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+
+        // Define ColorDrawable object and parse color
+        // using parseColor method
+        // with color hash code as its parameter
+        ColorDrawable colorDrawable
+                = new ColorDrawable(Color.parseColor("#000000"));
+
+        // Set BackgroundDrawable
+        actionBar.setBackgroundDrawable(colorDrawable);
+
+
 
         Intent intent = getIntent();
         String poster = intent.getStringExtra("Extra_poster");
